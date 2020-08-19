cc17ab5d9c7162c877c98b52e2c5b5be8f444525
Debasish Sarker <debasishsarker36@gmail.com>
2020-08-19 21:02:17 +0600

    instance veriable and class veriable done

--- instance_veriable_vs_class_veriable/employee.py
+++ instance_veriable_vs_class_veriable/employee.py
@@ -1,7 +1,10 @@
+
 class Employee:
 
     # class veriable
     numOfEmployees = 0
+    # can be a class and instance veriable
+    salaryRaise = 1.04
     
     # constractor method
     def __init__(self, firstName, lastName, salary):
@@ -11,8 +14,13 @@ class Employee:
         self.email = firstName + "." + lastName + "@company.com"
         self.salary = salary
         Employee.numOfEmployees += 1
+        print('Number Of employees: {} \n'.format(Employee.numOfEmployees))
 
-    def printEmployee(self):
+    def raiseSalary(self):
+        self.salary = self.salary * self.salaryRaise
+
+
+    def printEmployeeInfo(self):
         self.printFullName()
         self.printEmail()
         self.printSalary()
@@ -24,11 +32,31 @@ class Employee:
         print('Email: {} '.format(self.email))
 
     def printSalary(self):
-        print('Salary: {} '.format(self.salary))
+        print('Salary: {} \n'.format(self.salary))
+
+    
 
 
-print(Employee.numOfEmployees)
+
+
 emp1 = Employee("Debasish", "Sarker", 50000)
-emp1.printEmployee()
-print(Employee.numOfEmployees)
+emp1.printEmployeeInfo()
+
+emp2 = Employee("John", "Cena", 100000)
+emp2.printEmployeeInfo()
+
+# here salary raise will be used as an class veriable
+emp1.raiseSalary()
+emp1.printEmployeeInfo()
+
+# here salaryRaise becomes an instance veriable
+emp2.salaryRaise = 1.05
+emp2.raiseSalary()
+emp2.printEmployeeInfo()
+
+
+print(emp1.__dict__)
+emp1.salaryRaise = 1.05
+print(emp1.__dict__)
+
 
